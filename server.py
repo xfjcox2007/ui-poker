5f487b95a2b0c096985da0f95a7dbf090fd9bf10
bnp2111 <bnp2111@columbia.edu>
2022-04-17 21:21:01 -0400

    Added quiz and result templates

--- server.py
+++ server.py
@@ -4,17 +4,35 @@ import json
 
 app = Flask(__name__)
 current_dir = os.getcwd()
+
 teaching_file = open(current_dir+'/data/teaching.json')
 content = json.load(teaching_file)
 teaching_file.close()
+
+quiz_file = open(current_dir+'/data/quiz.json')
+questions = json.load(quiz_file)
+quiz_file.close()
+
 
 @app.route('/', )
 def home():
     return render_template('Home.html')
 
+
 @app.route('/learn/<page>')
 def teaching_blinds(page):
     return render_template("learn.html", content=content[page], total=len(content))
+
+
+@app.route('/quiz/<page>')
+def quiz_page(page):
+    return render_template("quiz.html", question=questions[page], total=len(questions), index=int(page))
+
+
+@app.route('/results')
+def results():
+    return render_template("results.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/quiz.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  if (question.type === 'mcq') {
+    let selectedOption = 0;
+    $('.mcq .choice:nth-child(1)').attr('selected', 'true');
+
+    $('.choice').click(function (e) {
+      selectedOption = $(this).index();
+      deselectAll();
+      console.log(selectedOption);
+      $(`.mcq .choice:nth-child(${selectedOption + 1})`).attr('selected', 'true');
+    });
+
+    function deselectAll() {
+      for (let i = 0; i < question.options.length; i++) {
+        $(`.mcq .choice`).removeAttr('selected');
+      }
+    }
+  } else if(question.type === 'arrange') {
+    let options = question.options;
+    let order = [...Array(options.length).keys()]
+
+    for(let option of options) {
+      $('.arrange').append(`<div class="arrange-option">${ option }</div>`)
+    }
+  }
+});
