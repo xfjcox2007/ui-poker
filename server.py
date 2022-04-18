8a37f5188e8ea99843a93aaae228645ec6c9047f
AdityaSatnalika0202 <89994257+AdityaSatnalika0202@users.noreply.github.com>
2022-04-18 12:49:09 -0400

    User Logs

--- server.py
+++ server.py
@@ -2,34 +2,47 @@ from flask import Flask, render_template, request
 import os
 import json
 import datetime
+import uuid
 
 app = Flask(__name__)
 current_dir = os.getcwd()
 
-teaching_file = open(current_dir+'/data/teaching.json')
+teaching_file = open(current_dir + '/data/teaching.json')
 content = json.load(teaching_file)
 teaching_file.close()
 
-quiz_file = open(current_dir+'/data/quiz.json')
+quiz_file = open(current_dir + '/data/quiz.json')
 questions = json.load(quiz_file)
 quiz_file.close()
 
-user_log = []
+user_details = open(current_dir + '/data/user.json')
+user = json.load(user_details)
+user_details.close()
+
+# user_details.id = uuid.uuid1()
+
+user_log = [user]
+
 
 @app.route('/', )
 def home():
+    user_log[0]["id"] = uuid.uuid4().int
+    print(user_log[0])
     return render_template('Home.html')
 
 
 @app.route('/learn/<page>')
 def teaching_blinds(page):
-    user_log.append({page: datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")})
+    user_log[0]["learn"] = {page: datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")}
     return render_template("learn.html", content=content[page], total=len(content))
 
 
 @app.route('/quiz/<page>')
 def quiz_page(page):
-    return render_template("quiz.html", question=questions[page], total=len(questions), index=int(page))
+    print(user_log[0])
+    user_log[0]["quiz"].append({page: datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")})
+    return render_template("quiz.html", question=questions[page], total=len(questions), index=int(page),
+                           logs=user_log[0]["quiz"])
 
 
 @app.route('/results')
--- static/quiz.js
+++ static/quiz.js
@@ -1,26 +1,27 @@
 $(document).ready(function () {
-  if (question.type === 'mcq') {
-    let selectedOption = 0;
-    $('.mcq .choice:nth-child(1)').attr('selected', 'true');
+    if (question.type === 'mcq') {
+        let selectedOption = 0;
+        $('.mcq .choice:nth-child(1)').attr('selected', 'true');
 
-    $('.choice').click(function (e) {
-      selectedOption = $(this).index();
-      deselectAll();
-      console.log(selectedOption);
-      $(`.mcq .choice:nth-child(${selectedOption + 1})`).attr('selected', 'true');
-    });
+        $('.choice').click(function (e) {
+            selectedOption = $(this).index();
+            deselectAll();
+            console.log(selectedOption);
+            logs["answer"] = selectedOption
+            $(`.mcq .choice:nth-child(${selectedOption + 1})`).attr('selected', 'true');
+        });
 
-    function deselectAll() {
-      for (let i = 0; i < question.options.length; i++) {
-        $(`.mcq .choice`).removeAttr('selected');
-      }
+        function deselectAll() {
+            for (let i = 0; i < question.options.length; i++) {
+                $(`.mcq .choice`).removeAttr('selected');
+            }
+        }
+    } else if (question.type === 'arrange') {
+        let options = question.options;
+        let order = [...Array(options.length).keys()]
+
+        for (let option of options) {
+            $('.arrange').append(`<div class="arrange-option">${option}</div>`)
+        }
     }
-  } else if(question.type === 'arrange') {
-    let options = question.options;
-    let order = [...Array(options.length).keys()]
-
-    for(let option of options) {
-      $('.arrange').append(`<div class="arrange-option">${ option }</div>`)
-    }
-  }
 });
