1547aff016822d4e49310020013aaa44923230bc
xfjcox2007 <mindyxu776@gmail.com>
2022-04-18 21:10:42 -0400

    add more data for teaching part

--- server.py
+++ server.py
@@ -32,13 +32,14 @@ def home():
 
 @app.route('/learn/<page>')
 def teaching_blinds(page):
-    user_log[0]["learningProgress"].append({page: datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")})
+    # user_log[0]["learningProgress"] = {page: datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")}
+    # user_log[0]["learningProgress"].append({page: datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")})
     return render_template("learn.html", content=content[page], total=len(content))
 
 
 @app.route('/quiz/<page>')
 def quiz_page(page):
-    user_log[0]["quiz"][page] = {"startTime": datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")}
+    # user_log[0]["quiz"][page] = {"startTime": datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S")}
     return render_template("quiz.html", question=questions[page], total=len(questions), index=int(page),
                            logs=user_log[0]["quiz"])
 
